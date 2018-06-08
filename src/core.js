8462a175d81f813bdfc8f9c7b247c878b7408912
Hatollint <hatollint@gmail.com>
2018-06-08 17:09:07 +0300

    Update core.js

--- src/core.js
+++ src/core.js
@@ -26,36 +26,31 @@ function ReplaceAll(str, search, replacement) {
 }
 
 function compile(text, lang) {
-    /* text - текст для реплейса
-     * lang - язык текста ('pon' or 'pas')
-     */
+    // lang - язык текста ('pon' or 'pas')
     let commentRegExp = /((?:\/\*(?:[^*]|(?:\*+[^*\/]))*\*+\/)|(?:\/\/.*))/g;
     let tmpToken = 'pon_' + (new Date()).getTime() + '_';
     let rStringLiterals = {};
+    
     text = text.replace(/\"(?:\\.|[^\"\\])*\"|\'(?:\\.|[^\'\\])*\'/g, function (val, pos) {
         let needKey = tmpToken + pos;
         rStringLiterals[needKey] = val;
         return needKey;
     });
     let commentsArray = text.match(commentRegExp) || [];
+    
     text = iterateText(text, lang);
-    // comeback comments
+    
     text = text.replace(commentRegExp, function () {
         return commentsArray.shift();
     });
-    // comeback strings
+    
     for (tmpToken in rStringLiterals) {
         text = text.replace(tmpToken, rStringLiterals[tmpToken]);
     }
-    // text = yoptTransliterateFunctionsNames(text);
+    
     return text;
 }
 
-
-/**
- * @param text текст, по которому следует пройтись
- * @param to язык текста ('pon' or 'pas')
- */
 function iterateText(text, to = 'pas') {
     let lang = LANGS[to];
 

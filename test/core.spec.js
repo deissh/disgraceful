09b763dae0505ce6a3d59ff62c61e335749d514a
Hatollint <hatollint@gmail.com>
2018-06-09 16:49:31 +0300

    Update core.spec.js

--- test/core.spec.js
+++ test/core.spec.js
@@ -5,16 +5,8 @@ const core = require('../src/core');
 
 
 describe('core', () => {
-    it('can compile to pas', () => {
-        cases.map(([pas, pon]) => {
-            chai.assert.equal(core.compile(pon, "pon"), pon);
-        });
-    });
-    it('can decompile from pas', () => {
-        cases.map(([pas, pon]) => {
-            chai.assert.equal(core.compile(pas, "pas"), pas);
-        });
-    });
+    it('can compile to pas', () => cases.map(([pas, pon]) => chai.assert.equal(core.compile(pon, "pon"), pon)));
+    it('can decompile from pas', () => cases.map(([pas, pon]) => chai.assert.equal(core.compile(pas, "pas"), pas)));
 });
 
 

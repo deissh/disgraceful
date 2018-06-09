e5fa2949166180e3fb9598ceb7962995b96a1ba4
Hatollint <hatollint@gmail.com>
2018-06-09 16:46:07 +0300

    Update core.spec.js

--- test/core.spec.js
+++ test/core.spec.js
@@ -8,12 +8,12 @@ describe('core', () => {
     it('can compile to pas', () => {
         cases.map(([pas, pon]) => {
             chai.assert.equal(core.compile(pon, "pon"), pon);
-        })
+        });
     });
     it('can decompile from pas', () => {
         cases.map(([pas, pon]) => {
-            chai.assert.equal(core.compile(pas, "pas"), pas)
-        })
+            chai.assert.equal(core.compile(pas, "pas"), pas);
+        });
     });
 });
 
